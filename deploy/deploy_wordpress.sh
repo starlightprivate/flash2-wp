fb136e209e240cbc3e39e5013a5deb323d4489ee
Vladislav Tomenko <vladislavtomenko@gmail.com>
2017-03-23 17:15:30 +0700

    feat: add wordpress deployment

---
+++ deploy/deploy_wordpress.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+export IMAGE_TAG="${CI_COMMIT_ID}"
+export IMAGE_NAME="wordpress"
+export APPLICATION_NAME="wordpress"
+
+# authenticate to google cloud
+codeship_google authenticate
+
+# set compute zone 
+gcloud config set compute/zone ${CLUSTER_ZONE}
+
+# set kubernetes cluster
+gcloud container clusters get-credentials "${CLUSTER_NAME}"
+
+# install envsubst
+apt-get install gettext-base -y
+
+# update kubernetes Deployment file
+envsubst < deploy/kubernetes/wordpress_deployment.yml.template > deploy/kubernetes/wordpress_deployment.yml
+
+cat deploy/kubernetes/wordpress_deployment.yml
+
+# deploy
+deployment_flag=$(GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl get deployment -l app=${APPLICATION_NAME})
+
+if [[ -z "$deployment_flag" ]]; then
+  echo "Create new deployment and service for ${APPLICATION_NAME}"
+  GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl create -f deploy/kubernetes/wordpress_deployment.yml
+  #GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl expose deployment ${APPLICATION_NAME} --name=${APPLICATION_NAME} --port=80,443 --type="LoadBalancer"
+else
+  echo "Rolling update for ${APPLICATION_NAME}"
+  GOOGLE_APPLICATION_CREDENTIALS=/keyconfig.json kubectl apply -f deploy/kubernetes/wordpress_deployment.yml
+fi
